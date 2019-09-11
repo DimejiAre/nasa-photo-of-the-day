47b912ffce9f1d9ff92d1d2caec295693c222d43
DimejiAre <olardi2@gmail.com>
2019-09-11 13:12:08 +0100

    add effect hook for API call side effect and create state for result

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 import Header from "./components/Header";
 import Image from "./components/Image";
 import Footer from "./components/Footer";
 
 function App() {
+
+  const [data, setData] = useState(null)
+
+  useEffect(()=>{
+    axios.get('https://lambda-github-api-server.herokuapp.com/')
+    .then(response => {
+      setData(response.data)
+    })
+  },[])
+
+  console.log(data)
+
   return (
     <div className="App App-header">
       <Header />
