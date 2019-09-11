a6d809705b8ff83a6f37e55139eea1066ad8990e
DimejiAre <olardi2@gmail.com>
2019-09-11 14:28:11 +0100

    clean up code

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,7 @@ function Image(props){
     return (
         <div className='main-content'>
             <h2>{title}</h2>
-            <div class="image"><img src={imageUrl} alt='Photo of the day'/></div>
+            <div><img src={imageUrl} alt='Photo of the day'/></div>
             <p>{explanation}</p>
         </div>
     )
