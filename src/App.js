aa293369c0b1d165c81e7eea830a06cc4f2825ca
DimejiAre <olardi2@gmail.com>
2019-09-11 12:55:48 +0100

    import components and add test data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import Image from "./components/Image";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className="App App-header">
+      <Header />
+      <Image 
+      title='Test title'
+      ImageUrl='Test Url'
+      explanation='No explanation'/>
+      <Footer 
+      copyright='Dimeji'
+      date='11/11/11'/>
     </div>
   );
 }
