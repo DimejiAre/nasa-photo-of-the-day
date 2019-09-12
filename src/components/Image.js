5012f281337fa4314ee5f9a32c76e6cda37fa635
DimejiAre <olardi2@gmail.com>
2019-09-12 13:54:38 +0100

    add styled-component for Image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,13 +1,24 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledImage = styled.div`
+    p {
+        padding: 0 40px;
+        margin: 5px 0;
+    }
+    img {
+        max-height: 700px;
+    }
+`;
 
 function Image(props){
     const {title, imageUrl, explanation} = props;
     return (
-        <div className='main-content'>
+        <StyledImage className='main-content'>
             <h2>{title}</h2>
             <div><img src={imageUrl} alt='Photo of the day'/></div>
             <p>{explanation}</p>
-        </div>
+        </StyledImage>
     )
 }
 
