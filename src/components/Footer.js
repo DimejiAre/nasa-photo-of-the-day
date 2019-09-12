09dceb36ae166f8574ed2f5eb96910b9c87d8e33
DimejiAre <olardi2@gmail.com>
2019-09-12 13:54:11 +0100

    add styled-component for Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+    background-color: cornflowerblue;
+    padding: 10px 0;
+`;
 
 function Footer(props){
     const {copyright, date} = props;
     return (
-        <footer>
+        <StyledFooter>
             <p>Published: {date}</p>
             <p>Image Credits & CopyRight: {copyright}</p>
-        </footer>
+        </StyledFooter>
     )
 }
 
