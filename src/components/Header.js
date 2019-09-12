009dcdf33e53fde603e3aa6663ec124343b5c7fa
DimejiAre <olardi2@gmail.com>
2019-09-12 13:53:52 +0100

    add styled-component for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from "react";
+import styled from "styled-components"
+
+const StyledHeader = styled.header`
+    background-color: cornflowerblue;
+    padding: 10px 0;
+    h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+`;
 
 function Header(){
     return (
-        <header>
+        <StyledHeader>
             <h1>NASA PHOTO OF THE DAY</h1>
-        </header>
+        </StyledHeader>
     )
 }
 
